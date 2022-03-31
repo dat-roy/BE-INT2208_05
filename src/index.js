01e136d9d4385e1c24c3ff7f754c2ec1dff78026
lehuyhaianh0808@gmail.com <lehuyhaianh0808@gmail.com>
2022-03-31 23:35:23 +0700

    logIn_sign_up_function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const jose = require('jose');
 
+const AuthRoute = require('../routes/auth');
+
+var bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+
+//mongoose 
+mongoose.connect('mongodb://localhost:27017/mydb');
+var db = mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function (callback) {
+    console.log("connection succeeded");
+})
+
 
 //Google Auth
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 const { stringify } = require('querystring');
 const CLIENT_ID = '836271056493-9jkcpgrhn8qur3f65vvchuksj2m4ub1t.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
@@ -19,9 +33,46 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));          //Serving static files in express
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, 'view/index.html'));
+//tạo tuyến đường cho signin function
+app.use('/api', AuthRoute);
+
+app.post('/sign_up', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var pass = req.body.password;
+    var phone = req.body.phone;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "password": pass,
+        "phone": phone
+
+    }
+    db.collection('details').insertOne(data, function (err, collection) {
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+
+    });
+
+    return res.sendFile(path.join(__dirname, 'view/signup_success.html'));
+})
+
+app.get('/', function (req, res) {
+    res.set({
+        'Access-control-Allow-Origin': '*'
+    });
+    return res.sendFile(path.join(__dirname, 'view/login.html'));
+})
+
+
+app.get('/trang-chu', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view/index.html'));
 })
 
 app.post('/', (req, res) => {
@@ -40,16 +91,16 @@ app.post('/', (req, res) => {
         const userid = payload['sub'];
         // If request specified a G Suite domain:
         // const domain = payload['hd'];
-      }
+    }
     verify()
-        .then(()=>{
+        .then(() => {
             res.cookie('session-token', token);
             res.send('success');
         })
         .catch(console.error);
 })
 
-app.get('/logou', (req, res)=>{
+app.get('/logout', (req, res) => {
     res.clearCookie('session-token');
     res.redirect('/');
 })
