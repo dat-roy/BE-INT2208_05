fd45856359f8e7e23d0be5f3f0462100246a0dec
dat-roy <dat_r0y@outlook.com>
2022-04-01 11:01:14 +0700

    version 1.0.0

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const jose = require('jose');
+const bodyParser = require('body-parser');
 
 
 //Google Auth
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 const { stringify } = require('querystring');
 const CLIENT_ID = '836271056493-9jkcpgrhn8qur3f65vvchuksj2m4ub1t.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
@@ -18,16 +18,23 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));          //Serving static files in express
+// for parsing application/json
+app.use(bodyParser.json()); 
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'view/index.html'));
 })
 
-app.post('/', (req, res) => {
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view/login.html'));
+})
 
-    token = req.body.token;
-    //console.log(req.body.token);
+app.post('/login', (req, res) => {
+
+    token = req.body.credential;
 
     let user = {};
     async function verify() {
@@ -44,12 +51,12 @@ app.post('/', (req, res) => {
     verify()
         .then(()=>{
             res.cookie('session-token', token);
-            res.send('success');
+            res.sendFile(path.join(__dirname, 'view/login.html'));
         })
         .catch(console.error);
 })
 
-app.get('/logou', (req, res)=>{
+app.get('/logout', (req, res)=>{
     res.clearCookie('session-token');
     res.redirect('/');
 })
