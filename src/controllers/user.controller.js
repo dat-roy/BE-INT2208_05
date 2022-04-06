67fcd21bc875bccf9c1693f31dea73bb30646aec
dat-roy <dat_r0y@outlook.com>
2022-04-06 08:09:49 +0700

    modify forgot-password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -108,110 +108,88 @@ class userController {
     forgotPassword(req, res, next) {
         const JWT_SECRET = 'some super secret';
 
-        let user = {
-            id: "ahdfksjdldfa",
-            email: "lvdat.roy@gmail.com",
-            password: "",
-        };
+        UserModel.findOne({email: req.body.email})
+            .then(user => {
+                
+                const secret = JWT_SECRET + user.password;
+                const payload = {
+                    email: user.email,
+                    id: user.id,
+                };
+                let token = jwt.sign(payload, secret, {expiresIn: '15m'});
+                const link = `http://localhost:3000/user/reset-password/${user.id}/${token}`;
+                //console.log(link);
 
-        const { email } = req.body;
-        if (email !== user.email) {
-            res.send("User not registered");
-        }
+                // create reusable transporter object using the default SMTP transport
+                let transporter = nodemailer.createTransport({
+                    service: "gmail",   
+                    auth: {
+                        user: "group7.int2208@gmail.com", 
+                        pass: "nhom7isthebest",
+                    },
+                });
 
-        const secret = JWT_SECRET + user.password;
-        const payload = {
-            email: user.email,
-            id: user.id,
-        };
+                let info = {
+                    from: "Tiro Accounts",                  
+                    to: `${user.email}`,                         
+                    subject: "Bạn Có Thư Tình???",          
+                    text: link,                             // plain text body
+                };
 
-        const token = jwt.sign(payload, secret, {expiresIn: '15m'})
-        const link = `http://localhost:3000/user/reset-password/${user.id}/${token}`
-        //console.log(link);
-        res.send('Password reset link has been sent to your email...')
-
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: "gmail",   
-            auth: {
-                user: "group7.int2208@gmail.com", 
-                pass: "nhom7isthebest",
-            },
-        });
-
-        let info = {
-            from: "Tiro Accounts",       // sender address
-            to: `${email}`,                         // list of receivers
-            subject: "Bạn Có Thư Tình???",                    // subject line
-            text: link,                             // plain text body
-        };
-
-        transporter.sendMail(info, (err, data) => {
-            if (err) {
-                console.log('Error: ', err);
-            } else {
-                console.log('Email sent');
-            }
-        });
-
+                transporter.sendMail(info, (err, data) => {
+                    if (err) {
+                        console.log('Error: ', err);
+                    } else {
+                        console.log('Email sent');
+                    }
+                });
+                res.send('Password reset link has been sent to your email...');
+            })
+            .catch(err => {
+                //console.log(err);
+            });
     }
 
     // [GET] /user/reset-password/:id/:token
     viewResetPassword(req, res, next) {
         const JWT_SECRET = 'some super secret';
 
-        let user = {
-            id: "ahdfksjdldfa",
-            email: "lvdat.roy@gmail.com",
-            password: "",
-        };
-
-        const { id, token } = req.params;
-    
-        if (id !== user.id) {
-            res.send('Invalid id...');
-        }
-
-        const secret = JWT_SECRET + user.password;
-        try {
-            const payload = jwt.verify(token, secret);
-            res.render('reset-password', {email: user.email});
-        } catch (error) {
-            console.log(error.message);
-            res.send(error.message);
-        }
+        var {token} = req.params;
+        UserModel.findOne({id: req.params.id})
+            .then(user => {
+                const secret = JWT_SECRET + user.password;
+                try {
+                    const payload = jwt.verify(token, secret);
+                    res.render('reset-password', {email: user.email});
+                } catch (err) {
+                    //console.log(err.message);
+                    res.send(err.message);
+                }
+            })
     }
     
     // [POST] /user/reset-password/:id/:token
     resetPassword(req, res, next) {
         const JWT_SECRET = 'some super secret';
-        let user = {
-            id: "ahdfksjdldfa",
-            email: "lvdat.roy@gmail.com",
-            password: "",
-        };
+        UserModel.findOne({id: req.params.id})
+            .then( user => {
+                const { password, confirm_password } = req.body;
+                if (password !== confirm_password) {
+                    return;
+                } else if (password.length <2) {
+                    return;
+                }
 
-        const { id, token} = req.params;
-        const { password, confirm_password } = req.body;
-        if (id !== user.id) {
-            res.send('Invalid id...');
-        }
-
-        if (password !== confirm_password) {
-            return;
-        } else if (password.length <2) {
-            return;
-        }
-
-        const secret = JWT_SECRET + user.password;
-        try {
-            const payload = jwt.verify(token, secret)
-            user.password = password;
-            res.send(user);
-        } catch (error) {
-            console.log(error.message);
-            res.send(error.message);
-        }
+                const secret = JWT_SECRET + user.password;
+                try {
+                    const payload = jwt.verify(req.params.token, secret)
+                    user.password = password;
+                    res.send(user);
+                } catch (error) {
+                    console.log(error.message);
+                    res.send(error.message);
+                }
+            })
     }
 
     // [GET] /user/profile
