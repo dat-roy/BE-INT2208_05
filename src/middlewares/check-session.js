91e4ec3c0076487f3b37ec6f45af394288a92751
dat-roy <dat_r0y@outlook.com>
2022-04-26 17:24:51 +0700

    modify check-session

--- src/middlewares/check-session.js
+++ src/middlewares/check-session.js
@@ -1,26 +1,29 @@
 const UserModel = require('../models/user.model.js');
 const jwt = require('jsonwebtoken');
+const { response } = require('express');
 
 function checkSession(req, res, next) {
     const JWTPrivateKey = "TiroAccounts";
 
     let token = req.cookies['session-token'];
-    let user = {};
-    async function verify() {
+    try {
         //Default algorithm: HMAC SHA256
         const payload = jwt.verify(token, JWTPrivateKey);
-        //UserModel.findOne({ email: payload.email})
-        user.email = payload.email;
-        //console.log(user);
-    }
-        verify()
-        .then(() => {
-            req.user = user;
-            next();
+        UserModel.findOne({ email: payload.email})
+        .then(user => {
+            if (user.email_verified == true) {
+                req.user = user;
+                next();
+            } else {
+                res.send('Your account has not been activated.');
+            }
         })
         .catch(() => {
-            res.send('Chưa đăng nhập mà đòi xem???');
+            res.send('Your account does not exist.');
         })
+    } catch (err) {
+        res.send('Please login first!');
+    }
 }
 
 module.exports = checkSession;
