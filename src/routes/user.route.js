6df92ac065d97dd79bf13d9bc18fdf99ae43640a
dat-roy <dat_r0y@outlook.com>
2022-05-10 15:33:30 +0700

    .

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -13,14 +13,15 @@ router.get('/forgot-password', userController.viewForgotPassword);
 router.get('/profile', checkSession, userController.renderProfile);
 router.get('/settings', checkSession, userController.renderUserSettings);
 router.get('/protected-route', checkSession, userController.renderProtectedRoute);      
+router.get('/reset-password/:id/:token', userController.viewResetPassword);
 
+//Server
 router.post('/register', userController.submitRegister);
 router.post('/register-with-google', userController.submitRegisterWithGoogle);
 router.get('/activate-account/:token', userController.activateAccount);
 router.post('/auth/login', userController.verifyLogin);
 router.post('/auth/google-login', userController.verifyGoogleLogin);
 router.post('/forgot-password', userController.forgotPassword);
-router.get('/reset-password/:id/:token', userController.viewResetPassword);
 router.post('/reset-password/:id/:token', userController.resetPassword);
 router.post('/settings', checkSession, upload('avatar').single('avatar'), userController.saveUserSettings);
 router.get('/logout', userController.logout);
