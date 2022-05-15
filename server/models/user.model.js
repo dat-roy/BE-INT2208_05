02bb0a254e5d0d204a37746856db9b4bd192a987
dat-roy <dat_r0y@outlook.com>
2022-05-15 22:24:41 +0700

    modify user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,20 +5,18 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema(
     {
-        username: { type: String, default: '' },
+        username: { type: String, default: '', unique: true },
         given_name: { type: String, default: '' },
         family_name: { type: String, default: '' },
         email: { type: String, default: '', unique: true },
-        email_verified: { type: Boolean, default: 'false' },
         phone: { type: String, default: '' },
-        password: { type: String, default: '' },
         picture: { 
-            name: {type: String, default: 'default.jpg',},
+            name: {type: String, default: '',},
             image_url: {type: Boolean, default: 'false'},
         },
         role: {type: String, default: ''},
-        gender: {type: String, default: "other"},
-        address: {type: String, default: ""},
+        gender: {type: String, default: 'other'},
+        address: {type: String, default: ''},
         theme: {type: String, default: ''},
         slug: { type: String, slug: 'email', unique: true },
     },
