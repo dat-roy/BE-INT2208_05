a7d3107722d3c49ed82474c9d3a4d800c4236a6b
dat-roy <dat_r0y@outlook.com>
2022-05-16 00:09:09 +0700

    remove email_verified

--- server/middlewares/check-session.js
+++ server/middlewares/check-session.js
@@ -4,17 +4,13 @@ const JWTPrivateKey = process.env.JWT_PRIVATE_KEY;
 
 function checkSession(req, res, next) {
     let token = req.cookies['session-token'];
-    console.log(token);
     try {
         //Default algorithm: HMAC SHA256
         const payload = jwt.verify(token, JWTPrivateKey);
         UserModel.findOne({ email: payload.email })
             .then(user => {
                 if (!user) {
-                    return res.status(404).json({ message: 'Your account does not exist.' });
-                }
-                if (!user.email_verified) {
-                    return res.status(403).json({ message: 'Your account has not been activated.' });
+                    return res.status(404).json({ message: `Your account does not exist.` });
                 }
                 if (!user.picture.image_url) {
                     user.picture.name = `/upload/avatar/${user.picture.name}`;
