9b279e3948ee8b0b280c651720d3db8a9fc5f04b
dat-roy <dat_r0y@outlook.com>
2022-05-15 23:06:44 +0700

    add qr code

--- server/controllers/site.controller.js
+++ server/controllers/site.controller.js
@@ -1,7 +1,31 @@
+const QRcode = require('qrcode');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {FRONTEND_HOST, FE_PORT} = process.env;
+
 class siteController {
     // [GET] /
     home(req, res) {
         res.render('index');
+    }
+    // [POST] /share/qr
+    shareByQR(req, res) {
+        const url = `http://${FRONTEND_HOST}:${FE_PORT}`;
+        QRcode.toDataURL(url, function (err, qr_url) {    
+            if (err) {
+                res.status(500).json({
+                    message: `Error when generate a QR code`,
+                    error: err,
+                })
+            } else {
+                //res.send('<img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src=' + qr_url + '>');
+                res.status(200).json({
+                    message: `Generate a QR code successfully`,
+                    qr_url: qr_url,
+                })
+            }
+        });
     }
 }
 
--- server/routes/site.route.js
+++ server/routes/site.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const siteController = require('../controllers/site.controller.js');
 
-router.get('/home', siteController.home);
 router.get('/', siteController.home);
+router.get('/share/qr', siteController.shareByQR);
 
 module.exports = router;
