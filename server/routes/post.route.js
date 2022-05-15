0ef467fd9fcd80664992385859cf1ef5251bd42a
dat-roy <dat_r0y@outlook.com>
2022-05-15 22:24:04 +0700

    modify routes

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -4,10 +4,10 @@ const postController = require('../controllers/post.controller.js');
 const checkSession = require('../middlewares/check-session.js');  
 
 //=================================
-router.post('/new-post', checkSession, postController.createNewPost);
-router.get('/my-posts', checkSession, postController.getMyPosts);
 router.post('/search', postController.searchPosts);
 router.get('/get/:id', postController.getPostById);
 router.get('/get-all', postController.getAllPosts)
+router.post('/new-post', checkSession, postController.createNewPost);
+router.get('/my-posts', checkSession, postController.getMyPosts);
 
 module.exports = router;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -5,25 +5,14 @@ let checkSession = require('../middlewares/check-session.js');
 let upload = require('../middlewares/upload-image');
 
 const saved_image_folder = 'avatar';
-const upload_image_field = "file";
-//=========================//
+const upload_image_field = 'file';
 
-//Views
-//router.get('/register', userController.register);
+//=========================//
+//Views 
+//TODO: remove in last version
 router.get('/login', userController.login);
-//router.get('/forgot-password', userController.viewForgotPassword);
-//router.get('/profile', checkSession, userController.renderProfile);
-//router.get('/settings', checkSession, userController.renderUserSettings);
-//router.get('/protected-route', checkSession, userController.renderProtectedRoute);      
-//router.get('/reset-password/:id/:token', userController.viewResetPassword);
 
 //Server
-//router.post('/register', userController.submitRegister);
-//router.post('/register-with-google', userController.submitRegisterWithGoogle);
-//router.get('/activate-account/:token', userController.activateAccount);
-//router.post('/auth/login', userController.verifyLogin);
-//router.post('/forgot-password', userController.forgotPassword);
-//router.post('/reset-password/:id/:token', userController.resetPassword);
 router.post('/auth/google-login', userController.verifyGoogleLogin);
 router.get('/logout', userController.logout);
 router.get('/get', checkSession, userController.getUserData);
