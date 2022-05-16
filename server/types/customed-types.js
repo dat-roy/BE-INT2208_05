17352e8f1bd409bfc677bcac937f9a440a5aa7ef
dat-roy <dat_r0y@outlook.com>
2022-05-16 14:42:34 +0700

    new customed types

---
+++ server/types/customed-types.js
@@ -0,0 +1,35 @@
+export const RoleOptions = Object.freeze({
+    RENTER: 'RENTER',
+    LANDLORD: 'LANDLORD',
+    ADMIN: 'ADMIN',
+});
+
+export const GenderOptions = Object.freeze({
+    MALE: 'MALE',
+    FEMALE: 'FEMALE',
+    OTHER: 'OTHER',
+    ALL: 'ALL',
+});
+
+export const ColorOptions = Object.freeze({
+
+});
+
+export const PostStatus = Object.freeze({
+    REJECTED: -1,
+    PENDING: 0,
+    APPROVED: 1,
+});
+
+export const RoomTypes = Object.freeze({
+
+});
+
+export const UtilitiesOptions = Object.freeze({
+
+});
+
+export const AccountStatus = Object.freeze({
+    NEW_ACCOUNT: 'NEW_ACCOUNT',
+    EXISTENT_ACCOUNT: 'EXISTENT_ACCOUNT',
+});
