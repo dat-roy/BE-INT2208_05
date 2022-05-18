b5e49abce7665e1ade76e27bab6cb53a9b85de58
dat-roy <dat_r0y@outlook.com>
2022-05-18 16:36:10 +0700

    add admin routes

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -4,6 +4,10 @@ const adminController = require('../controllers/admin.controller.js');
 const checkSession = require('../middlewares/check-session.js');  
 
 //===============
-router.get('/resolve-pending-posts/:id', )
+router.get('/get/pending-posts', checkSession, adminController.getPendingPosts);
+router.get('/get/approved-posts', checkSession, adminController.getApprovedPosts);
+router.get('/get/rejected-posts', checkSession, adminController.getRejectedPosts);
+router.get('/approve/:id', checkSession, adminController.setApproved);
+router.get('/reject/:id', checkSession, adminController.setRejected);
 
 module.exports = router;
